6fb6c4b824fe40e42c5fd1bd5d00e171dc8a2695
Dmytro Rui <ruy.dima@gmail.com>
2024-04-26 04:09:09 +0300

    use hash router

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import {routes} from "./routes/routes.tsx";
 import store, {persistor} from "./redux/redux-store.ts";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 function App() {
   return (
